5b9dba5dda983ac79416930227f4ce94a95b95b2
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-08-08 14:33:55 +0530

    Adding the data for users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
  import { Component } from '@angular/core';
  import { HeaderComponent } from './header/header.component';
-import { UserComponent } from "./user/user.component";
+ import { UserComponent } from "./user/user.component";
+
+
 
 @Component({
   selector: 'app-root',
