357737300f74e0d1708ceda86aa53d7bc1648e45
Shashank Pratheepkumar <Shashanksanthosh19@gmail.com>
2024-08-09 14:45:59 +0530

    making input required true to reduce the future bugs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,8 +11,8 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
   // selectedUser = signal(DUMMY_USERS[randomIndex]);
   get imagePath() {
     return 'assets/users/' + this.avatar;
